b549c8df6a7d9a2971a504b750b0658ef52557b0
石佳琪 <rt006814@rotai.com>
2022-09-16 13:49:23 +0800

    initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+/**
+ * @author ：sjq
+ * @date ：Created in ${DATE} ${TIME}
+ * @description：${description}
+ * @modified By：
+ * @version: $version$
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
